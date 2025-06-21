7ffcc1f7515c06b5930053f0863996a079c9510a
Nikhil <154459496+alphaplayerofdooms@users.noreply.github.com>
2025-06-21 13:14:26 +0530

    initial backend,frontend structure setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='bg-black flex items-center justify-center h-screen'>
+      <h1 className='text-white'>Congrats! React is set up.</h1>
+    </div>
+  );
+}
+
+export default App;

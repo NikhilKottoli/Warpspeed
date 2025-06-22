783e38a9aa210e6ab41cf552876c2ff00b9c5281
Nikhil <154459496+alphaplayerofdooms@users.noreply.github.com>
2025-06-22 10:10:37 +0530

    removed re-declaration of component and merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-
-import LandingPage from './components/LandingPage';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ethers } from 'ethers';
